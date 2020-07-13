7ea3c6bb079cd47fd51164379570f3ec967b2c6c
Yilong Chang <yilong@confluent.io>
2020-07-13 15:02:47 -0700

    Add override dec.

--- kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/exception/BigQueryConnectException.java
+++ kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/exception/BigQueryConnectException.java
@@ -62,6 +62,7 @@ public class BigQueryConnectException extends ConnectException {
     return messageBuilder.toString();
   }
 
+  @Override
   public String toString() {
     return getCause() != null ?
         super.toString() + "\nCaused by: " + getCause().getLocalizedMessage() : super.toString();
