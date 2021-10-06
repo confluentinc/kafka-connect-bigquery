30985523d720b935ff4a2ef3f11b1e822311fe1a
Canh Son Le (Sam) <81574307+samle-ovo@users.noreply.github.com>
2021-10-06 11:34:38 +0100

    remove space

--- kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/write/batch/TableWriter.java
+++ kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/write/batch/TableWriter.java
@@ -94,7 +94,7 @@ public class TableWriter implements Runnable {
           currentIndex += currentBatchSize;
           successCount++;
         } catch (BigQueryException err) {
-          logger.warn("Could not write batch of size {} to BigQuery table {}.", currentBatchList.size(),  table.getFullTableName(), err);
+          logger.warn("Could not write batch of size {} to BigQuery table {}.", currentBatchList.size(), table.getFullTableName(), err);
           if (isBatchSizeError(err)) {
             failureCount++;
             currentBatchSize = getNewBatchSize(currentBatchSize, err);
