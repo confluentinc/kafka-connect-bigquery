816d8e7a37c01ab543c631e95f1487bb2c883b42
Bhagyashree <bhagyashree@confluent.io>
2023-04-12 11:55:13 +0530

    refactored default stream test

--- kcbq-connector/src/test/java/com/wepay/kafka/connect/bigquery/write/storageApi/StorageWriteApiDefaultStreamTest.java
+++ kcbq-connector/src/test/java/com/wepay/kafka/connect/bigquery/write/storageApi/StorageWriteApiDefaultStreamTest.java
@@ -13,6 +13,7 @@ import io.grpc.StatusRuntimeException;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.json.JSONObject;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentMatchers;
 
@@ -22,7 +23,7 @@ import static org.mockito.Mockito.CALLS_REAL_METHODS;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.doNothing;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,34 +32,36 @@ import java.util.concurrent.ExecutionException;
 public class StorageWriteApiDefaultStreamTest {
 
     TableName mockedTableName = TableName.of("dummyProject", "dummyDataset", "dummyTable");
-
+    JsonStreamWriter mockedStreamWriter = mock(JsonStreamWriter.class);
+    SinkRecord mockedSinkRecord = mock(SinkRecord.class);
+    ApiFuture<AppendRowsResponse> mockedResponse = mock(ApiFuture.class);
+    List<Object[]> testRows = Collections.singletonList(new Object[]{mockedSinkRecord, new JSONObject()});
+    StorageWriteApiDefaultStream defaultStream = mock(StorageWriteApiDefaultStream.class, CALLS_REAL_METHODS);
+    private final String nonRetriableExpectedException  = "Failed to write rows on table "
+            + mockedTableName.toString()
+            + " due to I am non-retriable error";
+    private final String retriableExpectedException = "Exceeded 0 attempts to write to table "
+            + mockedTableName.toString() + " ";
+    private final String malformedrequestExpectedException = "Insertion failed at table dummyTable for following rows:" +
+            " \n [row index 18] (Failure reason : f0 field is unknown) ";
+    @Before
+    public void setUp() throws Exception {
+        doReturn(mockedStreamWriter).when(defaultStream).getDefaultStream(ArgumentMatchers.any());
+        when(mockedStreamWriter.append(ArgumentMatchers.any())).thenReturn(mockedResponse);
+        doNothing().when(defaultStream).waitRandomTime();
+    }
     @Test
     public void testDefaultStreamNoExceptions() throws Exception {
-        JsonStreamWriter mockedStreamWriter = mock(JsonStreamWriter.class);
-        SinkRecord mockedSinkRecord = mock(SinkRecord.class);
-        ApiFuture<AppendRowsResponse> mockedResponse = mock(ApiFuture.class);
         AppendRowsResponse successResponse = AppendRowsResponse.newBuilder()
                 .setAppendResult(AppendRowsResponse.AppendResult.newBuilder().getDefaultInstanceForType()).build();
-        List<Object[]> testRows = new ArrayList<>();
-
-        testRows.add(new Object[]{mockedSinkRecord, new JSONObject()});
-
-        StorageWriteApiDefaultStream defaultStream = mock(StorageWriteApiDefaultStream.class, CALLS_REAL_METHODS);
-
-        doReturn(mockedStreamWriter).when(defaultStream).getDefaultStream(ArgumentMatchers.any());
-
-        when(mockedStreamWriter.append(ArgumentMatchers.any())).thenReturn(mockedResponse);
+        
         when(mockedResponse.get()).thenReturn(successResponse);
 
         defaultStream.appendRows(mockedTableName, testRows, null);
-
     }
 
     @Test(expected = BigQueryStorageWriteApiConnectException.class)
     public void testDefaultStreamNonRetriableError() throws Exception {
-        JsonStreamWriter mockedStreamWriter = mock(JsonStreamWriter.class);
-        SinkRecord mockedSinkRecord = mock(SinkRecord.class);
-        ApiFuture<AppendRowsResponse> mockedResponse = mock(ApiFuture.class);
         AppendRowsResponse nonRetriableError = AppendRowsResponse.newBuilder()
                 .setError(
                         Status.newBuilder()
@@ -66,31 +69,14 @@ public class StorageWriteApiDefaultStreamTest {
                                 .setMessage("I am non-retriable error")
                                 .build()
                 ).build();
-        List<Object[]> testRows = new ArrayList<>();
-        String expectedException = "Failed to write rows on table "
-                + mockedTableName.toString()
-                + " due to I am non-retriable error";
 
-        StorageWriteApiDefaultStream defaultStream = mock(StorageWriteApiDefaultStream.class, CALLS_REAL_METHODS);
-
-        doReturn(mockedStreamWriter).when(defaultStream).getDefaultStream(ArgumentMatchers.any());
-        when(mockedStreamWriter.append(ArgumentMatchers.any())).thenReturn(mockedResponse);
         when(mockedResponse.get()).thenReturn(nonRetriableError);
-        testRows.add(new Object[]{mockedSinkRecord, new JSONObject()});
 
-        try {
-            defaultStream.appendRows(mockedTableName, testRows, null);
-        } catch (Exception e) {
-            Assert.assertEquals(expectedException, e.getMessage());
-            throw e;
-        }
+        verifyException(nonRetriableExpectedException);
     }
 
     @Test(expected = BigQueryStorageWriteApiConnectException.class)
     public void testDefaultStreamRetriableError() throws Exception {
-        JsonStreamWriter mockedStreamWriter = mock(JsonStreamWriter.class);
-        SinkRecord mockedSinkRecord = mock(SinkRecord.class);
-        ApiFuture<AppendRowsResponse> mockedResponse = mock(ApiFuture.class);
         AppendRowsResponse retriableError = AppendRowsResponse.newBuilder()
                 .setError(
                         Status.newBuilder()
@@ -98,29 +84,14 @@ public class StorageWriteApiDefaultStreamTest {
                                 .setMessage("I am an INTERNAL error")
                                 .build()
                 ).build();
-        List<Object[]> testRows = new ArrayList<>();
-        String expectedException = "Exceeded 0 attempts to write to table " + mockedTableName.toString() + " ";
 
-        StorageWriteApiDefaultStream defaultStream = mock(StorageWriteApiDefaultStream.class, CALLS_REAL_METHODS);
-
-        doReturn(mockedStreamWriter).when(defaultStream).getDefaultStream(ArgumentMatchers.any());
-        when(mockedStreamWriter.append(ArgumentMatchers.any())).thenReturn(mockedResponse);
         when(mockedResponse.get()).thenReturn(retriableError);
-        testRows.add(new Object[]{mockedSinkRecord, new JSONObject()});
 
-        try {
-            defaultStream.appendRows(mockedTableName, testRows, null);
-        } catch (Exception e) {
-            Assert.assertEquals(expectedException, e.getMessage());
-            throw e;
-        }
+        verifyException(retriableExpectedException);
     }
 
     @Test(expected = BigQueryStorageWriteApiConnectException.class)
     public void testDefaultStreamMalformedRequestError() throws Exception {
-        JsonStreamWriter mockedStreamWriter = mock(JsonStreamWriter.class);
-        SinkRecord mockedSinkRecord = mock(SinkRecord.class);
-        ApiFuture<AppendRowsResponse> mockedResponse = mock(ApiFuture.class);
         AppendRowsResponse malformedError = AppendRowsResponse.newBuilder()
                 .setError(
                         Status.newBuilder()
@@ -129,89 +100,38 @@ public class StorageWriteApiDefaultStreamTest {
                                 .build()
                 ).addRowErrors(
                         RowError.newBuilder()
-                                .setIndex(5)
-                                .setMessage("f0 field name is unknown")
+                                .setIndex(18)
+                                .setMessage("f0 field is unknown")
                                 .build()
                 ).build();
-        List<Object[]> testRows = new ArrayList<>();
-        String expectedException = "Insertion failed at table dummyTable for following rows:" +
-                " \n [row index 5] (Failure reason : f0 field name is unknown) ";
 
-        StorageWriteApiDefaultStream defaultStream = mock(StorageWriteApiDefaultStream.class, CALLS_REAL_METHODS);
-
-        doReturn(mockedStreamWriter).when(defaultStream).getDefaultStream(ArgumentMatchers.any());
-        when(mockedStreamWriter.append(ArgumentMatchers.any())).thenReturn(mockedResponse);
         when(mockedResponse.get()).thenReturn(malformedError);
-        testRows.add(new Object[]{mockedSinkRecord, new JSONObject()});
 
-        try {
-            defaultStream.appendRows(mockedTableName, testRows, null);
-        } catch (Exception e) {
-            Assert.assertEquals(expectedException, e.getMessage());
-            throw e;
-        }
+        verifyException(malformedrequestExpectedException);
     }
-
-    // Exception block
 
     @Test(expected = BigQueryStorageWriteApiConnectException.class)
     public void testDefaultStreamNonRetriableException() throws Exception {
-        JsonStreamWriter mockedStreamWriter = mock(JsonStreamWriter.class);
-        SinkRecord mockedSinkRecord = mock(SinkRecord.class);
-        ApiFuture<AppendRowsResponse> mockedResponse = mock(ApiFuture.class);
         InterruptedException exception = new InterruptedException("I am non-retriable error");
-        List<Object[]> testRows = new ArrayList<>();
-        String expectedException = "Failed to write rows on table "
-                + mockedTableName.toString()
-                + " due to I am non-retriable error";
 
-        StorageWriteApiDefaultStream defaultStream = mock(StorageWriteApiDefaultStream.class, CALLS_REAL_METHODS);
-
-        doReturn(mockedStreamWriter).when(defaultStream).getDefaultStream(ArgumentMatchers.any());
-        when(mockedStreamWriter.append(ArgumentMatchers.any())).thenReturn(mockedResponse);
         when(mockedResponse.get()).thenThrow(exception);
-        testRows.add(new Object[]{mockedSinkRecord, new JSONObject()});
 
-        try {
-            defaultStream.appendRows(mockedTableName, testRows, null);
-        } catch (Exception e) {
-            Assert.assertEquals(expectedException, e.getMessage());
-            throw e;
-        }
+        verifyException(nonRetriableExpectedException);
     }
 
     @Test(expected = BigQueryStorageWriteApiConnectException.class)
     public void testDefaultStreamRetriableException() throws Exception {
-        JsonStreamWriter mockedStreamWriter = mock(JsonStreamWriter.class);
-        SinkRecord mockedSinkRecord = mock(SinkRecord.class);
-        ApiFuture<AppendRowsResponse> mockedResponse = mock(ApiFuture.class);
         ExecutionException exception = new ExecutionException(new StatusRuntimeException(
                 io.grpc.Status.fromCode(io.grpc.Status.Code.INTERNAL).withDescription("I am an INTERNAL error")
         ));
-        List<Object[]> testRows = new ArrayList<>();
-        String expectedException = "Exceeded 0 attempts to write to table "
-                + mockedTableName.toString() + " ";
 
-        StorageWriteApiDefaultStream defaultStream = mock(StorageWriteApiDefaultStream.class, CALLS_REAL_METHODS);
-
-        doReturn(mockedStreamWriter).when(defaultStream).getDefaultStream(ArgumentMatchers.any());
-        when(mockedStreamWriter.append(ArgumentMatchers.any())).thenReturn(mockedResponse);
         when(mockedResponse.get()).thenThrow(exception);
-        testRows.add(new Object[]{mockedSinkRecord, new JSONObject()});
 
-        try {
-            defaultStream.appendRows(mockedTableName, testRows, null);
-        } catch (Exception e) {
-            Assert.assertEquals(expectedException, e.getMessage());
-            throw e;
-        }
+        verifyException(retriableExpectedException);
     }
 
     @Test(expected = BigQueryStorageWriteApiConnectException.class)
     public void testDefaultStreamMalformedRequestException() throws Exception {
-        JsonStreamWriter mockedStreamWriter = mock(JsonStreamWriter.class);
-        SinkRecord mockedSinkRecord = mock(SinkRecord.class);
-        ApiFuture<AppendRowsResponse> mockedResponse = mock(ApiFuture.class);
         Map<Integer, String> errorMapping = new HashMap<>();
         errorMapping.put(18, "f0 field is unknown");
         Exceptions.AppendSerializtionError exception = new Exceptions.AppendSerializtionError(
@@ -219,48 +139,29 @@ public class StorageWriteApiDefaultStreamTest {
                 "Bad request",
                 "DEFAULT",
                 errorMapping);
-        List<Object[]> testRows = new ArrayList<>();
-        String expectedException = "Insertion failed at table dummyTable for following rows:" +
-                " \n [row index 18] (Failure reason : f0 field is unknown) ";
 
-        StorageWriteApiDefaultStream defaultStream = mock(StorageWriteApiDefaultStream.class, CALLS_REAL_METHODS);
-
-        doReturn(mockedStreamWriter).when(defaultStream).getDefaultStream(ArgumentMatchers.any());
-        when(mockedStreamWriter.append(ArgumentMatchers.any())).thenReturn(mockedResponse);
         when(mockedResponse.get()).thenThrow(exception);
-        testRows.add(new Object[]{mockedSinkRecord, new JSONObject()});
 
-        try {
-            defaultStream.appendRows(mockedTableName, testRows, null);
-        } catch (Exception e) {
-            Assert.assertEquals(expectedException, e.getMessage());
-            throw e;
-        }
+        verifyException(malformedrequestExpectedException);
     }
 
     @Test(expected = BigQueryStorageWriteApiConnectException.class)
     public void testDefaultStreamTableMissingException() throws Exception {
-        JsonStreamWriter mockedStreamWriter = mock(JsonStreamWriter.class);
-        SinkRecord mockedSinkRecord = mock(SinkRecord.class);
-        ApiFuture<AppendRowsResponse> mockedResponse = mock(ApiFuture.class);
         ExecutionException exception = new ExecutionException(new StatusRuntimeException(
                 io.grpc.Status
                         .fromCode(io.grpc.Status.Code.NOT_FOUND)
                         .withDescription("Not found: table. Table is deleted")
         ));
-        List<Object[]> testRows = new ArrayList<>();
         String expectedException = "Exceeded 30 attempts to write to table "
                 + mockedTableName.toString() + " ";
-        StorageWriteApiDefaultStream defaultStream = mock(StorageWriteApiDefaultStream.class, CALLS_REAL_METHODS);
 
-        testRows.add(new Object[]{mockedSinkRecord, new JSONObject()});
-
-        doReturn(mockedStreamWriter).when(defaultStream).getDefaultStream(ArgumentMatchers.any());
-        when(mockedStreamWriter.append(ArgumentMatchers.any())).thenReturn(mockedResponse);
         when(mockedResponse.get()).thenThrow(exception);
         when(defaultStream.getAutoCreateTables()).thenReturn(true);
-        doNothing().when(defaultStream).waitRandomTime();
 
+        verifyException(expectedException);
+    }
+
+    private void verifyException(String expectedException) {
         try {
             defaultStream.appendRows(mockedTableName, testRows, null);
         } catch (Exception e) {
