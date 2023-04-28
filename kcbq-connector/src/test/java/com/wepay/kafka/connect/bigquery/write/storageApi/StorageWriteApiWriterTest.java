ce5bba6765a326e0811a37b4eb7cc553142c1637
Bhagyashree <bhagyashree@confluent.io>
2023-04-28 16:46:50 +0530

    merging base changes

--- kcbq-connector/src/test/java/com/wepay/kafka/connect/bigquery/write/storageApi/StorageWriteApiWriterTest.java
+++ kcbq-connector/src/test/java/com/wepay/kafka/connect/bigquery/write/storageApi/StorageWriteApiWriterTest.java
@@ -1,8 +1,6 @@
 package com.wepay.kafka.connect.bigquery.write.storageApi;
 
 import static org.junit.Assert.assertEquals;
-
-import com.google.cloud.bigquery.storage.v1.TableName;
 import com.wepay.kafka.connect.bigquery.config.BigQuerySinkConfig;
 import com.wepay.kafka.connect.bigquery.config.BigQuerySinkTaskConfig;
 import com.wepay.kafka.connect.bigquery.convert.BigQueryRecordConverter;
@@ -19,11 +17,12 @@ import org.mockito.Mockito;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 
-import java.util.*;
+import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Optional;
 
 public class StorageWriteApiWriterTest {
     Schema keySchema = SchemaBuilder.struct().field("key", Schema.STRING_SCHEMA).build();
@@ -37,10 +36,8 @@ public class StorageWriteApiWriterTest {
     public void testRecordConversion() {
         StorageWriteApiBase mockStreamWriter = Mockito.mock(StorageWriteApiBase.class);
         BigQuerySinkTaskConfig mockedConfig = Mockito.mock(BigQuerySinkTaskConfig.class);
-        StorageApiBatchModeHandler batchModeHandler = mock(StorageApiBatchModeHandler.class);
         RecordConverter mockedRecordConverter = new BigQueryRecordConverter(false, false);
-        TableWriterBuilder builder = new StorageWriteApiWriter.Builder(
-                mockStreamWriter, null, mockedRecordConverter, mockedConfig, batchModeHandler);
+        TableWriterBuilder builder = new StorageWriteApiWriter.Builder(mockStreamWriter, null, mockedRecordConverter, mockedConfig);
         ArgumentCaptor<List<Object[]>> records = ArgumentCaptor.forClass(List.class);
         String expectedKafkaKey = "{\"key\":\"12345\"}";
         Set<String> expectedKeys = new HashSet<>();
@@ -62,45 +59,15 @@ public class StorageWriteApiWriterTest {
         assertEquals(1, records.getValue().size());
 
         JSONObject actual = (JSONObject) records.getValue().get(0)[1];
-        assertEquals(expectedKeys, actual.keySet());
+        assertEquals(expectedKeys,actual.keySet());
 
         String actualKafkaKey = actual.get("i_am_kafka_key").toString();
         assertEquals(expectedKafkaKey, actualKafkaKey);
     }
 
-    @Test
-    public void testBatchLoadStreamName() {
-        TableName tableName = TableName.of("p", "d", "t");
-        StorageWriteApiBase mockStreamWriter = Mockito.mock(StorageWriteApiBatchApplicationStream.class);
-        BigQuerySinkTaskConfig mockedConfig = Mockito.mock(BigQuerySinkTaskConfig.class);
-        StorageApiBatchModeHandler batchModeHandler = mock(StorageApiBatchModeHandler.class);
-        RecordConverter mockedRecordConverter = new BigQueryRecordConverter(
-                false, false);
-        ArgumentCaptor<String> streamName = ArgumentCaptor.forClass(String.class);
-        String expectedStreamName = tableName.toString() + "_s1";
-        TableWriterBuilder builder = new StorageWriteApiWriter.Builder(
-                mockStreamWriter, tableName, mockedRecordConverter, mockedConfig, batchModeHandler);
-
-
-        Mockito.when(mockedConfig.getKafkaDataFieldName()).thenReturn(Optional.empty());
-        Mockito.when(mockedConfig.getKafkaKeyFieldName()).thenReturn(Optional.of("i_am_kafka_key"));
-        Mockito.when(mockedConfig.getBoolean(BigQuerySinkConfig.SANITIZE_FIELD_NAME_CONFIG)).thenReturn(true);
-        when(batchModeHandler.updateOffsetsOnStream(any(), any())).thenReturn(expectedStreamName);
-
-        builder.addRow(createRecord("abc", 100), null);
-        builder.build().run();
-
-        verify(mockStreamWriter, times(1))
-                .initializeAndWriteRecords(any(), any(), streamName.capture());
-
-        assertEquals(expectedStreamName, streamName.getValue());
-
-
-    }
-
     private SinkRecord createRecord(String topic, long offset) {
         Object key = new Struct(keySchema).put("key", "12345");
-        Object value = new Struct(valueSchema)
+        Object value =new Struct(valueSchema)
                 .put("id", 1L)
                 .put("name", "1")
                 .put("available-name", true);
